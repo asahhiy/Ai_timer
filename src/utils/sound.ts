992bd0a3d2742fe7603c62e1ea7bb1d277d1b617
asahhiy <asahhiy@gmail.com>
2025-08-26 11:26:04 +0900

    updated

---
+++ src/utils/sound.ts
@@ -0,0 +1,9 @@
+export async function playNotificationSound() {
+  try {
+    //try catch構文の活用
+    const audio = new Audio('');
+  } catch (error) {
+    console.error('failed play sound effect!', error);
+    j
+  }
+}
